017886efcc34ef656803588801d6009997fdb97c
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-07-30 12:56:43 +0530

    Add files via upload

---
+++ BackTracking/Hard/332. Reconstruct Itinerary.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+from typing import List
+
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        adj= {src: [] for src, dest in tickets} 
+        tickets.sort()
+        for src, dest in tickets:
+            adj[src].append(dest)
+        
+        res = ["JFK"]
+        def dfs(src): 
+            if len(res) == len(tickets) + 1:
+                return True
+            if src not in adj:
+                return False
+            temp = list(adj[src])
+            for i , v in enumerate(temp):
+                adj[src].pop(i)
+                res.append(v)
+
+                if dfs(v):return True
+                adj[src].index(i,v)
+                res.pop()
+            return False
+
+        dfs("JFK")
+        return res
+
+
+s = Solution()
+print(s.findItinerary(tickets = [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]))
+
