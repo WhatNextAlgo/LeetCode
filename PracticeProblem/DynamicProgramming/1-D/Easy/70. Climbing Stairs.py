69d566d3ea15f31150e566008406abd9ddd04610
Sumit Maurya <sumitmaurya201.sm@gmail.com>
2022-10-16 11:45:03 +0530

    Create 70. Climbing Stairs.py

---
+++ PracticeProblem/DynamicProgramming/1-D/Easy/70. Climbing Stairs.py
@@ -0,0 +1,15 @@
+class Solution:
+    # fibonacci series
+    def climbStairs(self, n: int) -> int:
+        one , two = 1,1
+        for i in range(n -1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
+
+    
+             
+
+s = Solution()
+print(s.climbStairs(n = 5))
